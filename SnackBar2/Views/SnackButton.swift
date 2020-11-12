fc6bc5fc0dcbf0b0615b8586064de0c4689c5ac2
TakuWan849 <wantaku339@gmail.com>
2020-11-13 06:17:04 +0900

    Buttonの位置調整

--- SnackBar2/Views/SnackButton.swift
+++ SnackBar2/Views/SnackButton.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class SnackButton: UIButton {
     
+    let myColor = Colors()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -22,11 +24,11 @@ final class SnackButton: UIButton {
     private func SetUpButton() {
         
         backgroundColor = UIColor.white
-        setTitleColor( UIColor(displayP3Red: 79/255, green: 172/255, blue: 254/255,alpha: 1.0), for: .normal)
-        titleLabel?.font = UIFont.boldSystemFont(ofSize: 14.0)
-        contentEdgeInsets = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
-        layer.cornerRadius = 15.0
-        layer.borderColor =  UIColor(displayP3Red: 79/255, green: 172/255, blue: 254/255,alpha: 1.0).cgColor
+        setTitleColor( myColor.skyBlue, for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        contentEdgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        layer.cornerRadius = 20
+        layer.borderColor =  myColor.skyBlue.cgColor
         layer.borderWidth = 2
         
     }
