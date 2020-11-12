d66af38b7a44d524b6e1c80f4049654f8c36db71
TakuWan849 <wantaku339@gmail.com>
2020-11-13 06:18:05 +0900

    Colorの外部ファイル化

---
+++ SnackBar2/Resources/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  SnackBar2
+//
+//  Created by 野澤拓己 on 2020/11/13.
+//
+
+import UIKit
+
+struct Colors {
+    
+    // blue系
+    let skyBlue = UIColor(displayP3Red: 79/255, green: 172/255, blue: 254/255,alpha: 1.0)
+    
+    //
+}
